b48b2cfd05ef0909c67d15ef822d15df7f1b06f9
AlanLM18 <alanlm.ti23@utsjr.edu.mx>
2026-06-09 18:15:51 -0600

    Catalogo de cursos - Alan

--- demo/src/main/java/com/example/equipo/repository/CursoRepository.java
+++ demo/src/main/java/com/example/equipo/repository/CursoRepository.java
@@ -1,8 +1,13 @@
 package com.example.equipo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.equipo.entity.Cursos;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CursoRepository extends JpaRepository<Cursos, Integer> {
 
+    Page<Cursos> findByActivoTrue(Pageable pageable);
 }
